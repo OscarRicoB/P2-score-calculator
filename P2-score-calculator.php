6c4a9698800799342cd0371e4664167d61792ccd
OscarRicoB <oscarricoba@gmail.com>
2020-02-05 12:26:49 -0600

    Problem solution

--- P2-score-calculator.php
+++ P2-score-calculator.php
@@ -1,2 +1,31 @@
 <?php
 //Starting test
+define("FILE_ROUTE","test-files/");
+//Define the file to read
+$file_to_decrypt = isset($_POST['score_file']) ? $_POST['score_file']:(isset($_GET['score_file'])? $_GET['score_file']:"file1.txt");
+
+$advantage = [
+  "p1" => [],
+  "p2" => [],
+];
+
+$fh = @fopen(FILE_ROUTE.$file_to_decrypt,'r');
+$ln = (int)rtrim(fgets($fh));
+
+for ($i=0; $i < $ln; $i++) {
+  $tempScore = explode(" ", rtrim(fgets($fh)));
+  // var_dump($tempScore);
+  $higher = array_keys($tempScore,max($tempScore))[0] + 1;
+  $advantage["p".$higher][] = abs($tempScore[0] - $tempScore[1]);
+  // echo "p{$higher} is the winner of the round with ".abs($tempScore[0] - $tempScore[1])." advantage<hr>";
+}
+
+// var_dump($advantage);
+// echo "<hr>max of p1 = ".max($advantage["p1"]);
+// echo "<hr>max of p2 = ".max($advantage["p2"]);
+
+if ( max($advantage["p1"]) > max($advantage["p2"]) ) {
+  echo "1 ".max($advantage["p1"]) ;
+}else {
+  echo "2 ".max($advantage["p1"]) ;
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # P2-score-calculator
 El programa recibe los marcadores y deberá indicar el ganador (no hay empates, se puede asumir siempre existe un ganador único)
+<br>
+<br>
+Como ejecutarlo en entorno local
+si por defecto queremos llamar el programa con el archivo ejemplo "file1.txt" se llamara la url de la siguiente manera:<br>
+http://localhost/P2-score-calculator/P2-score-calculator.php
+si queremos probar con un archivo diferente, sera necesario colocar dicho archivo en la carpeta "test-files/" y pasar el nombre del archivo en el request,
+el nombre del archivo puede venir en POST o GET "score_file". Ejemplo en la isguiente url mandaremos verificar el archivo "file2.txt":<br>
+http://localhost/P2-score-calculator/P2-score-calculator.php?score_file=file2.txt
